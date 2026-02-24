a040609cb3102c285a0a10c7961a8c7c7913be0c
carlschader <carlschader@proton.me>
2026-02-24 05:12:14 -0500

    sync

--- stardew_types.py
+++ stardew_types.py
@@ -16,14 +16,30 @@ class Season(StrEnum):
     FALL = "Fall"
     WINTER = "Winter"
 
-class Price(NamedTuple):
-    standard: int
-    silver: int
-    gold: int
-
 class SeedInfo(NamedTuple):
     seed_type: SeedType
+    cost: int
     growth_time: int
-    season: Season
+    seasons: list[Season]
     xp: int
-    sell_price: Price
+    sell_price: int 
+    continued_growth_time: int | None = None
+
+    @property
+    def silver_price(self) -> float:
+        return float(self.sell_price) * 1.25
+
+    @property
+    def gold_price(self) -> float:
+        return float(self.sell_price) * 1.5
+
+    @property
+    def iridium_price(self) -> float:
+        return float(self.sell_price) * 2.0
+
+    def compound_investment_multiple(self) -> float:
+        if self.continued_growth_time is None:
+            return self.silver_price / self.cost
+        else:
+            total_growth_time = self.growth_time + self.continued_growth_time
+            return (self.silver_price * 2) / (self.cost * 2)
